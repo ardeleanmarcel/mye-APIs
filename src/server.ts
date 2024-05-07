00592429ed301c1e01eb86b7616ae1d76818f1ad
Valle <aharapu2@yahoo.com>
2024-05-07 14:14:20 +0300

    Create trpc server

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import { fastifyTRPCPlugin, FastifyTRPCPluginOptions } from '@trpc/server/adapters/fastify';
+import fastify from 'fastify';
+import { createContext } from './trpcFastifyContext';
+import { appRouter, type AppRouter } from './trpcAppRouter';
+
+const server = fastify({
+  maxParamLength: 5000,
+});
+
+server.register(fastifyTRPCPlugin, {
+  prefix: '/trpc',
+  trpcOptions: {
+    router: appRouter,
+    createContext,
+    onError({ path, error }) {
+      // report to error monitoring
+      console.error(`Error in tRPC handler on path '${path}':`, error);
+    },
+  } satisfies FastifyTRPCPluginOptions<AppRouter>['trpcOptions'],
+});
+
+(async () => {
+  try {
+    console.log('starting server on port 3000');
+    await server.listen({ port: 3000 });
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+})();
