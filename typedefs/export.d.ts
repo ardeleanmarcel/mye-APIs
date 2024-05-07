f9b4e72b770dfb159af94865a6ad671f6bf522a6
Valle <aharapu2@yahoo.com>
2024-05-07 15:13:55 +0300

    update more configs

---
+++ typedefs/export.d.ts
@@ -0,0 +1,2 @@
+import type { AppRouter } from './trpcAppRouter';
+export { AppRouter };
---
+++ typedefs/trpcAppRouter.d.ts
@@ -0,0 +1,112 @@
+export declare const t: {
+    _config: import("@trpc/server").RootConfig<{
+        ctx: object;
+        meta: object;
+        errorShape: import("@trpc/server").DefaultErrorShape;
+        transformer: import("@trpc/server").DefaultDataTransformer;
+    }>;
+    procedure: import("@trpc/server").ProcedureBuilder<{
+        _config: import("@trpc/server").RootConfig<{
+            ctx: object;
+            meta: object;
+            errorShape: import("@trpc/server").DefaultErrorShape;
+            transformer: import("@trpc/server").DefaultDataTransformer;
+        }>;
+        _ctx_out: object;
+        _input_in: typeof import("@trpc/server").unsetMarker;
+        _input_out: typeof import("@trpc/server").unsetMarker;
+        _output_in: typeof import("@trpc/server").unsetMarker;
+        _output_out: typeof import("@trpc/server").unsetMarker;
+        _meta: object;
+    }>;
+    middleware: <TNewParams extends import("@trpc/server").ProcedureParams<import("@trpc/server").AnyRootConfig, unknown, unknown, unknown, unknown, unknown, unknown>>(fn: import("@trpc/server").MiddlewareFunction<{
+        _config: import("@trpc/server").RootConfig<{
+            ctx: object;
+            meta: object;
+            errorShape: import("@trpc/server").DefaultErrorShape;
+            transformer: import("@trpc/server").DefaultDataTransformer;
+        }>;
+        _ctx_out: {};
+        _input_out: typeof import("@trpc/server").unsetMarker;
+        _input_in: unknown;
+        _output_in: unknown;
+        _output_out: unknown;
+        _meta: object;
+    }, TNewParams>) => import("@trpc/server").MiddlewareBuilder<{
+        _config: import("@trpc/server").RootConfig<{
+            ctx: object;
+            meta: object;
+            errorShape: import("@trpc/server").DefaultErrorShape;
+            transformer: import("@trpc/server").DefaultDataTransformer;
+        }>;
+        _ctx_out: {};
+        _input_out: typeof import("@trpc/server").unsetMarker;
+        _input_in: unknown;
+        _output_in: unknown;
+        _output_out: unknown;
+        _meta: object;
+    }, TNewParams>;
+    router: <TProcRouterRecord extends import("@trpc/server").ProcedureRouterRecord>(procedures: TProcRouterRecord) => import("@trpc/server").CreateRouterInner<import("@trpc/server").RootConfig<{
+        ctx: object;
+        meta: object;
+        errorShape: import("@trpc/server").DefaultErrorShape;
+        transformer: import("@trpc/server").DefaultDataTransformer;
+    }>, TProcRouterRecord>;
+    mergeRouters: typeof import("@trpc/server").mergeRouters;
+    createCallerFactory: <TRouter extends import("@trpc/server").Router<import("@trpc/server").AnyRouterDef<import("@trpc/server").RootConfig<{
+        ctx: object;
+        meta: object;
+        errorShape: import("@trpc/server").DefaultErrorShape;
+        transformer: import("@trpc/server").DefaultDataTransformer;
+    }>, any>>>(router: TRouter) => import("@trpc/server").RouterCaller<TRouter["_def"]>;
+};
+export declare const appRouter: import("@trpc/server").CreateRouterInner<import("@trpc/server").RootConfig<{
+    ctx: object;
+    meta: object;
+    errorShape: import("@trpc/server").DefaultErrorShape;
+    transformer: import("@trpc/server").DefaultDataTransformer;
+}>, {
+    getUserById: import("@trpc/server").BuildProcedure<"query", {
+        _config: import("@trpc/server").RootConfig<{
+            ctx: object;
+            meta: object;
+            errorShape: import("@trpc/server").DefaultErrorShape;
+            transformer: import("@trpc/server").DefaultDataTransformer;
+        }>;
+        _meta: object;
+        _ctx_out: object;
+        _input_in: string;
+        _input_out: string;
+        _output_in: typeof import("@trpc/server").unsetMarker;
+        _output_out: typeof import("@trpc/server").unsetMarker;
+    }, {
+        id: string;
+        name: string;
+        bio?: string | undefined;
+    }>;
+    createUser: import("@trpc/server").BuildProcedure<"mutation", {
+        _config: import("@trpc/server").RootConfig<{
+            ctx: object;
+            meta: object;
+            errorShape: import("@trpc/server").DefaultErrorShape;
+            transformer: import("@trpc/server").DefaultDataTransformer;
+        }>;
+        _meta: object;
+        _ctx_out: object;
+        _input_in: {
+            name: string;
+            bio?: string | undefined;
+        };
+        _input_out: {
+            name: string;
+            bio?: string | undefined;
+        };
+        _output_in: typeof import("@trpc/server").unsetMarker;
+        _output_out: typeof import("@trpc/server").unsetMarker;
+    }, {
+        id: string;
+        name: string;
+        bio?: string | undefined;
+    }>;
+}>;
+export type AppRouter = typeof appRouter;
