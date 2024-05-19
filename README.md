27dbecc5b73e762fb2c4013c0a0eec7d9e9fc432
Valle <aharapu2@yahoo.com>
2024-05-19 16:35:28 +0300

    Confiugre DB conncetion using env vars

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 ## Local Development
 
+- Create `.env` file based on `.env.example` file
 - Start development server with `npm run dev`
--- src/db/client.ts
+++ src/db/client.ts
@@ -1,9 +1,10 @@
+import 'dotenv/config';
 import postgres from 'postgres';
 
 export const sql = postgres({
-  host: 'localhost',
-  port: 3033,
-  database: 'mye',
-  user: 'myeadmin',
-  pass: 'myepass',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT ?? ''),
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  pass: process.env.DB_PASS,
 });
