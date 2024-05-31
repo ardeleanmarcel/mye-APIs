5f5d89c48b5730a7a61dc6d0d9b4a7e0c4b98554
Valle <aharapu2@yahoo.com>
2024-05-31 19:54:08 +0300

    Update path resolvers

--- src/routers/users.ts
+++ src/routers/users.ts
@@ -1,11 +1,10 @@
-import { t } from '@src/trpc';
-
 import { hash } from 'bcrypt';
 import lodash from 'lodash';
 
-import { DEFAULT_SALT_ROUNDS } from '../constants/auth.const';
-import { userCreateSchema } from '../models/user.models';
-import { createUsers } from '../db/sql/users.sql';
+import { t } from '@src/trpc';
+import { DEFAULT_SALT_ROUNDS } from '@constants/auth.const';
+import { userCreateSchema } from '@models/user.models';
+import { createUsers } from '@sql/users.sql';
 
 const { pick } = lodash;
 
@@ -17,9 +16,7 @@ export const users = t.router({
     const { username, password } = opts.input;
 
     const hashedPassword = await hash(password, DEFAULT_SALT_ROUNDS);
-
     const user = (await createUsers([{ username, password: hashedPassword }]))[0];
-
     const data = pick(user, ['user_id', 'username']);
 
     return data;
