413518745fdb0ccf798a4038353d07e52a17a299
Valle <aharapu2@yahoo.com>
2024-07-06 07:51:58 +0300

    44: Remove 'postgres' client and library

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,15 +1,5 @@
 import 'dotenv/config';
-import postgres from 'postgres';
 import knex from 'knex';
-
-// TODO (Valle) -> remove 'postgres' library and replace usage with knex
-export const sql = postgres({
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT ?? '5432'),
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  pass: process.env.DB_PASS,
-});
 
 export const knexClient = knex({
   client: 'pg',
