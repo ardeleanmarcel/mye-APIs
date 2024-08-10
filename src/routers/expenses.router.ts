b6aeb56410cb562cbbaa14ee85be7209e94f0481
Valle <aharapu2@yahoo.com>
2024-08-10 18:03:42 +0300

    Send new expenses query to db

--- src/routers/expenses.router.ts
+++ src/routers/expenses.router.ts
@@ -1,34 +1,13 @@
-import { z } from 'zod';
-
 import { protectedProcedure, t } from '@src/trpc';
-
-export const addExpenseSchema = z
-  .object({
-    description: z.string().min(3).max(50),
-    amount: z
-      .number()
-      .positive()
-      .lt(1_000_000_000_000)
-      .refine((num) => {
-        const asString = String(num);
-        const decimals = asString.split('.')[1];
-        return !decimals || decimals.length < 3;
-      }, 'amount should be a number with maximum 2 decimals'),
-    date_expended_at: z.string().date(),
-  })
-  .strict();
+import { expenseCreateSchema } from '@src/models/expense.models';
+import { createExpenses } from '@src/db/sql/expenses.sql';
 
 export const expensesRouter = t.router({
-  add: protectedProcedure.input(addExpenseSchema).mutation(async (opts) => {
+  create: protectedProcedure.input(expenseCreateSchema).mutation(async (opts) => {
     const { user } = opts.ctx;
 
-    const { amount, date_expended_at, description } = opts.input;
+    const result = createExpenses(opts.input, user.user_id);
 
-    console.log('user', user);
-    console.log('amount', amount);
-    console.log('date_expended_at', date_expended_at);
-    console.log('description', description);
-
-    return 'okie';
+    return result;
   }),
 });
